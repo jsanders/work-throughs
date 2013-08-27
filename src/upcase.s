d00197ee97f0e64880eb895af4f242de6d89d423
James Sanders <sanderjd@gmail.com>
2013-08-27 14:30:55 -0600

    Second version of upcase - read into a buffer

--- src/upcase.s
+++ src/upcase.s
@@ -3,6 +3,7 @@
 
 section .bss
 	Buffer: resb 1		; byte buffer
+	BufferSize: equ 1024
 
 section .data
 
@@ -14,28 +15,36 @@ Read:
 	mov eax, 3		; `sys_read` syscall
 	mov ebx, 0		; Use stdin
 	mov ecx, Buffer		; Read into buffer
-	mov edx, 1		; Only read one byte
+	mov edx, BufferSize	; Read BufferSize bytes
 	int 0x80		; Make syscall
+	mov esi, eax		; Number of characters read in esi, for later
 
 	cmp eax, 0		; Have we reached the end of the file?
 	je Exit			; If so, exit
 
-	cmp byte [Buffer], 0x61	; Is char below 'a'?
-	jb Write		; If so, don't upcase
+	mov ecx, esi		; Number of characters read in ecx
+	mov ebp, Buffer		; Address of Buffer
+	dec ebp			; Avoid off-by-one
 
-	cmp byte [Buffer], 0x7A	; Is char above 'z'?
-	ja Write		; If so, don't upcase
+Scan:
+	cmp byte [ebp+ecx], 0x61; Is char below 'a'?
+	jb Next			; If so, don't upcase
 
-	sub byte [Buffer], 0x20	; Subtract 0x20 from current byte to upcase it
+	cmp byte [ebp+ecx], 0x7A; Is char above 'z'?
+	ja Next			; If so, don't upcase
+
+	sub byte [ebp+ecx], 0x20; Subtract 0x20 from current byte to upcase it
+
+Next:
+	dec ecx			; Decrement ecx
+	jnz Scan		; If not zero, keep scanning
 
 Write:
 	mov eax, 4		; `sys_read` syscall
 	mov ebx, 1		; Use stdout
 	mov ecx, Buffer		; Write byte from buffer
-	mov edx, 1		; Only write one byte
+	mov edx, esi		; Write number of characters that were read
 	int 0x80		; Make syscall
-
-	jmp Read		; Read next byte
 
 Exit:
 	mov eax, 1		; `exit` syscall
