b412850c2dbb408aeeca5958d4b2d1fe3a2d6a49
James Sanders <sanderjd@gmail.com>
2013-08-27 23:03:15 -0600

    Use procedures in hexdump program

--- src/hexdump.asm
+++ src/hexdump.asm
@@ -11,31 +11,65 @@ SECTION .data
 	Digits db "0123456789ABCDEF"
 
 SECTION .text
-global _start
 
-_start:
-Read:	
+; ReadBuf: Reads BufLen bytes from stdin into Buf
+; Returns: Number of bytes read in esi
+; Modfies: esi, Buf
+ReadBuf:	
+	; Save caller registers
+	push eax
+	push ebx
+	push ecx
+	push edx
+
 	mov eax, 3		; `sys_read` syscall
 	mov ebx, 0		; Use stdin
 	mov ecx, Buf		; Read into buffer
 	mov edx, BufLen		; Read BufferSize bytes
 	int 0x80		; Make syscall
 
-	mov esi, eax		; Number of characters read in esi, for later
+	mov esi, eax		; Return number of characters read in esi
 
-	cmp eax, 0		; Have we reached the end of the file?
-	je Exit			; If so, exit
+	; Restore caller registers
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
 
-	mov ebp, Buf		; Address of Buffer
-	mov edi, HexStr		; Address of line string
+WriteHexStr:
+	; Save caller registers
+	push eax
+	push ebx
+	push ecx
+	push edx
 
-	xor ecx, ecx		; Clear ecx
+	mov eax, 4		; `sys_write` syscall
+	mov ebx, 1		; Use stdout
+	mov ecx, HexStr		; Write byte from buffer
+	mov edx, HexLen		; Write number of characters that were read
+	int 0x80		; Make syscall
 
-Scan:
+	; Restore caller registers
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+
+; PrintAtOffset: Print byte at given offset 
+; Input: Offset in ecx
+; Modifies: HexStr
+UpdateOffset:
+	; Save registers
+	push eax
+	push ebx
+	push edx
+
 	xor eax, eax		; Clear eax
 
 	; Put current character in eax and ebx
-	mov al, byte [ebp+ecx]
+	mov al, byte [Buf+ecx]
 	mov ebx, eax
 
 	; Isolate high nybble
@@ -59,21 +93,40 @@ Scan:
 	mov byte [HexStr+edx], bl	; Write high nybble char
 	mov byte [HexStr+edx+1], al	; Write low nybble char
 
-	; Go to next character and see if we're done
-	inc ecx			; Increment line string pointer
-	cmp ecx, esi		; Compare with number of chars
-	jna Scan		; Loop back if ecx is <= number of chars
-
-	; Write the line to stdout
-	mov eax, 4		; `sys_read` syscall
-	mov ebx, 1		; Use stdout
-	mov ecx, HexStr		; Write byte from buffer
-	mov edx, HexLen		; Write number of characters that were read
-	int 0x80		; Make syscall
-	jmp Read		; Get another buffer
+	; Restore registers
+	pop edx
+	pop ebx
+	pop eax
+	ret
 
 Exit:
 	mov eax, 1		; `exit` syscall
 	mov ebx, 0		; Exit code 0
 	int 0x80		; Exit
+	ret
+
+global _start
+_start:
+ReadLine:
+	call ReadBuf
+	cmp esi, 0		; Have we reached the end of the file?
+	je Exit			; If so, exit
+
+	mov ebp, Buf		; Address of Buffer
+	mov edi, HexStr		; Address of line string
+
+	xor ecx, ecx		; Clear ecx
+
+ScanLine:
+	call UpdateOffset
+
+	; Go to next character and see if we're done
+	inc ecx			; Increment line string pointer
+	cmp ecx, esi		; Compare with number of chars
+	jna ScanLine		; Loop back if ecx is <= number of chars
+
+	call WriteHexStr
+	jmp ReadLine		; Get another buffer
+
+	call Exit
 
