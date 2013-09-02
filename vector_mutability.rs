c68c0ad5dcbc0911fbb1936739d35bbbf4e09bf9
James Sanders <sanderjd@gmail.com>
2013-09-02 17:47:41 -0600

    Further vector mutability example

--- vector_mutability.rs
+++ vector_mutability.rs
@@ -1,7 +1,7 @@
 fn main() {
-  let a_vector = @[1,2,3];
-  let mut another_vector = @[];
-  another_vector = another_vector + a_vector;
+  let a_vector = ~[1,2,3];
+  let mut mut_vector = a_vector;
+  mut_vector[0] = 5;
 
-  println(fmt!("The first number is %d.", another_vector[0]));
+  println(fmt!("The first number is %d.", mut_vector[0]));
 }
