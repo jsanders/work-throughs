7f76bf20ddcdda9089528fc0899cfedecbe6590c
James Sanders <sanderjd@gmail.com>
2013-09-03 00:08:36 -0600

    Better guessing game

--- guessing_game.rs
+++ guessing_game.rs
@@ -8,40 +8,39 @@ fn generate_secret_number() -> int {
   return abs(rand::rng().gen::<int>() % 100) + 1;
 }
 
-fn process_guess(secret: int, guess: int, guesses: &mut int) {
+fn process_guess(secret: int, guess: int) -> bool {
   println(fmt!("You guessed: %d", guess));
 
   if guess > secret {
     println("Your guess was too high!");
+    false
   } else if guess < secret {
     println("Your guess was too low!");
-  } else if guess == secret {
+    false
+  } else {
     println("You got it!");
-    *guesses = 4;
+    true
   }
-
-  *guesses += 1;
 }
 
 fn main() {
   let secret = generate_secret_number();
 
-  let guesses = @mut 1;
-
   println("--- N U M B E R - G A M E ---");
   println("");
   println("Guess a number from 1-100 (you get five tries):");
 
-  loop {
-    println(fmt!("Guess #%d", *guesses));
+  for round in range(1, 6) {
+    println(fmt!("Guess #%d", round));
 
     let input = stdin().read_line();
 
     match int::from_str(input) {
-      Some(number) => process_guess(secret, number, guesses),
+      Some(number) => {
+        if process_guess(secret, number) { break; }
+      },
       None         => println("Hey, put in a number.")
     }
-    if *guesses == 5 { break; }
   }
 
   println("Done!");
